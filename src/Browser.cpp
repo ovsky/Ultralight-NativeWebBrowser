c44b31631ae15590ab8ffec72dc2ec579b87a40c
ovsky <przemek.orlowski@outlook.com>
2025-10-28 02:51:18 +0100

    Add ability to use dark navbar on Windows

--- src/Browser.cpp
+++ src/Browser.cpp
@@ -5,6 +5,11 @@
 #include <Ultralight/Renderer.h>
 #include <windows.h>
 
+#if defined(_WIN32)
+#include <dwmapi.h>
+#pragma comment(lib, "dwmapi.lib")
+#endif
+
 Browser::Browser()
 {
   Settings settings;
@@ -15,6 +20,17 @@ Browser::Browser()
   window_ = Window::Create(app_->main_monitor(), 1024, 768, false,
                            kWindowFlags_Resizable | kWindowFlags_Titled | kWindowFlags_Maximizable);
   window_->SetTitle("Ultralight | Web Browser");
+
+#if defined(_WIN32)
+  HWND hwnd = (HWND)window_->native_handle();
+  if (hwnd) {
+    BOOL use_dark_mode = TRUE;
+    DwmSetWindowAttribute(hwnd, 20, &use_dark_mode, sizeof(use_dark_mode)); // DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+
+    COLORREF dark_purple = RGB(42, 33, 60);
+    DwmSetWindowAttribute(hwnd, 35, &dark_purple, sizeof(dark_purple)); // DWMWA_CAPTION_COLOR = 35
+  }
+#endif
 
   // Set window icons using Win32 API
   HINSTANCE hInstance = GetModuleHandle(NULL);
