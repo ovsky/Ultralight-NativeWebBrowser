d50cd8cbf8973a55e0717c894c3fadd86d3c825e
ovsky <przemek.orlowski@outlook.com>
2025-12-02 20:32:54 +0100

    Add WebKit environment variable setup to main
    Introduces SetWebKitEnvironment to set environment variables that attempt to relax WebKit security restrictions. This is called at startup for both Windows and non-Windows entry points to potentially improve compatibility or debugging with Ultralight's WebKit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // Entry point for Windows; portable fallback for non-Windows.
 #include "Browser.h"
+#include <cstdlib>
 
 #define ENABLE_PAUSE_FOR_DEBUGGER 0
 
@@ -10,11 +11,26 @@ static void PauseForDebugger() { MessageBoxA(NULL, "Pause", "Caption", MB_OKCANC
 static void PauseForDebugger() {}
 #endif
 
+// Set environment variables to try to relax WebKit security (may not work with Ultralight's WebKit)
+static void SetWebKitEnvironment() {
+#if defined(_WIN32)
+  _putenv_s("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+  // Try to disable web security (these are WebKit env vars, may not work)
+  _putenv_s("WEBKIT_DISABLE_WEB_SECURITY", "1");
+  _putenv_s("WEBKIT_ALLOW_UNIVERSAL_ACCESS_FROM_FILE_URLS", "1");
+#else
+  setenv("WEBKIT_DISABLE_COMPOSITING_MODE", "1", 1);
+  setenv("WEBKIT_DISABLE_WEB_SECURITY", "1", 1);
+  setenv("WEBKIT_ALLOW_UNIVERSAL_ACCESS_FROM_FILE_URLS", "1", 1);
+#endif
+}
+
 #if defined(_WIN32)
 #include <Windows.h>
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
   PauseForDebugger();
+  SetWebKitEnvironment();
   Browser browser;
   browser.Run();
   return 0;
@@ -23,6 +39,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 int main(int argc, char **argv)
 {
   PauseForDebugger();
+  SetWebKitEnvironment();
   Browser browser;
   browser.Run();
   return 0;
